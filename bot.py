8d7aa2b4d12aa3a72cf6fd1f0ea64df185a563ba
Itzmepapa123 <absorbinghunger@gmail.com>
2024-06-19 07:58:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,14 +10,9 @@ import sys
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT
+
 import pyrogram.utils
-
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
-
-name ="""
- BY MIKEY FROM TG
-"""
-
 
 class Bot(Client):
     def __init__(self):
@@ -51,6 +46,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/weebs_support for support")
                 sys.exit()
+
         if FORCE_SUB_CHANNEL2:
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
@@ -64,10 +60,11 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/weebs_support for support")
                 sys.exit()
+
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
@@ -77,30 +74,33 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-        self.LOGGER(__name__).info(f""" \n\n       
-                                                   
-                  
-                                 
 
-  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
- / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
-| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
- \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
-                                                         
- 
-                                                                        
-                                                                      
-                                                                                 
-                              
-                                          """)
+        self.LOGGER(__name__).info("""
+          ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
+         / __/ _ \\|   \\| __| __| |  |_ _\\ \\/ / _ )/ _ \\_   _/ __|
+        | (_| (_) | |) | _|| _|| |__ | | >  <| _ \\ (_) || | \\__ \\
+         \\___\\___/|___/|___|_| |____|___/_/\\_\\___/\\___/ |_| |___/
+        """)
+
         self.username = usr_bot_me.username
-        #web-response
-        app = web.AppRunner(await web_server())
-        await app.setup()
+
+        # Web server setup for health checks
+        app = web.Application()
+        app.router.add_get('/health', self.health_check)
+
+        runner = web.AppRunner(app)
+        await runner.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        site = web.TCPSite(runner, bind_address, PORT)
+        await site.start()
+
+    async def health_check(self, request):
+        return web.Response(text="OK")
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
-            
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
